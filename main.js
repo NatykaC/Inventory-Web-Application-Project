88f6899d202af3801862d69b74634f54ee5531f4
Natyka Callwood <Natykacallwood@pursuit.org>
2023-07-02 20:31:31 -0400

    Updated the inventory.html file and changed the remove buttons from submit types to p tags with id's of 'cookie-remove', 'brownie-remove', and 'cake-remove'. In the main.js file added event listeners to the three products so that when you click 'Remove' on each product the product is removed. The product returns when you refresh the page.

--- main.js
+++ main.js
@@ -1,31 +1,48 @@
-    let cakeInStock = true;  
-    const cakeSubmit = document.querySelector("#cake-submit");
-        cakeSubmit.addEventListener("click", (event) =>{
-            cakeInStock = !cakeInStock;
-                cakeInStock ?
-                    cakeSubmit.innerText = "In Stock"
-                    :   
-                    cakeSubmit.innerText = "Out of Stock"
-
+let cakeInStock = true;  
+const cakeSubmit = document.querySelector("#cake-submit");
+    cakeSubmit.addEventListener("click", (event) =>{
+        cakeInStock = !cakeInStock;
+            cakeInStock ?
+                cakeSubmit.innerText = "In Stock"
+                :   
+                cakeSubmit.innerText = "Out of Stock"
                    
-        })
+    });
 
-    let brownieInStock = true;
-    const brownieSubmit = document.querySelector("#brownie-submit");
-        brownieSubmit.addEventListener("click", (event) =>{
-            brownieInStock = !brownieInStock;
-                brownieInStock ?
+let brownieInStock = true;
+const brownieSubmit = document.querySelector("#brownie-submit");
+    brownieSubmit.addEventListener("click", (event) =>{
+        brownieInStock = !brownieInStock;
+            brownieInStock ?
                 brownieSubmit.innerText = "In Stock"
                 :
                 brownieSubmit.innerText = "Out of Stock"
-        })
+    });
 
-    let cookieInStock = true;
-    const cookieSubmit = document.querySelector("#cookie-submit");
-        cookieSubmit.addEventListener("click", (event) =>{
-            cookieInStock = !cookieInStock;
+let cookieInStock = true;
+const cookieSubmit = document.querySelector("#cookie-submit");
+    cookieSubmit.addEventListener("click", (event) =>{
+        cookieInStock = !cookieInStock;
             cookieInStock ?
-            cookieSubmit.innerText = "In Stock"
-            :
-            cookieSubmit.innerText = "Out of Stock"
-        })
+                cookieSubmit.innerText = "In Stock"
+                :
+                cookieSubmit.innerText = "Out of Stock"
+    });
+
+const coconutCake = document.querySelector(".cake");
+const removeCake = document.querySelector("#cake-remove");
+    removeCake.addEventListener("click", (event) =>{
+        coconutCake.remove()
+    });
+
+const marbleBrownie = document.querySelector(".brownies");
+const removeBrownie = document.querySelector("#brownie-remove");
+    removeBrownie.addEventListener("click", (event) =>{
+        marbleBrownie.remove()
+    });
+
+const rockyRoadCookies = document.querySelector(".cookies");
+const removeCookies = document.querySelector("#cookie-remove");
+    removeCookies.addEventListener("click", (event) => {
+        rockyRoadCookies.remove()
+    });
